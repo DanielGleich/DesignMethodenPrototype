91c23e70e79b09dd5401cee4a925fe4f6a43ff93
Daniel Gleich <gleichdaniel16@gmail.com>
2024-05-20 20:09:34 +0200

    TestScene update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditorInternal;
@@ -5,33 +6,65 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] Transform orientation;
+    [SerializeField] Transform player;
+    [SerializeField] Transform playerObj;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] CinemachineFreeLook camera;
+
+
+    [Header("Parameters")]
     [SerializeField] private float moveSpeed;
-    private Rigidbody rb;
+    [SerializeField] private float rotationSpeed;
     // Start is called before the first frame update
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    Vector3 moveDirection;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Vorne Hinten ; Vertical
+        int axisZ = 0, axisX = 0;
         if (Input.GetKey("w"))
+            axisZ = 1;
+        else if (Input.GetKey("s"))
+            axisZ = -1;
+
+        // Links Rechts ; Horinzontal
+        if (Input.GetKey("a"))
+            axisX = -1;
+        else if (Input.GetKey("d"))
+            axisX = 1;
+
+        moveDirection = new Vector3(axisX, 0, axisZ);
+        Vector3 viewDir = player.position - new Vector3(camera.transform.position.x, player.position.y, camera.transform.position.z);
+        orientation.forward = viewDir.normalized;
+
+        Vector3 inputDir = orientation.forward * axisZ + orientation.right * axisX;
+        if (inputDir != Vector3.zero)
         {
-            rb.AddForce(-rb.transform.forward * moveSpeed * Time.deltaTime);
+            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
         }
-        else if (Input.GetKey("s")) { rb.AddForce(rb.transform.forward * moveSpeed * Time.deltaTime); }
-        
-        
-        if ( Input.GetKey("a") ) {
-            rb.AddForce(rb.transform.right * moveSpeed * Time.deltaTime);
-        } else if ( Input.GetKey("d") ) { rb.AddForce(-rb.transform.right * moveSpeed * Time.deltaTime);}
+        rb.AddForce(moveDirection * moveSpeed * Time.deltaTime, ForceMode.Force);
 
+        SpeedControl();
+    }
 
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }

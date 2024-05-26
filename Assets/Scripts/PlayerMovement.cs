e818a45d96df8ca0434ef6735b9367b6f6460340
Daniel Gleich <gleichdaniel16@gmail.com>
2024-05-27 01:08:11 +0200

    fixxed player controls with camera movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,12 +11,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Transform player;
     [SerializeField] Transform playerObj;
     [SerializeField] Rigidbody rb;
-    [SerializeField] CinemachineFreeLook camera;
+    //[SerializeField] CinemachineFreeLook camera;
+    Camera camera;
 
 
     [Header("Parameters")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotationSpeed;
+    [SerializeField] private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
     // Start is called before the first frame update
 
     Vector3 moveDirection;
@@ -24,6 +27,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        camera = Camera.main;
     }
 
     // Update is called once per frame
@@ -36,13 +40,14 @@ public class PlayerMovement : MonoBehaviour
         else if (Input.GetKey("s"))
             axisZ = -1;
 
-        // Links Rechts ; Horinzontal
+        // Links Rechts ; Horizontal
         if (Input.GetKey("a"))
             axisX = -1;
         else if (Input.GetKey("d"))
             axisX = 1;
 
-        moveDirection = new Vector3(axisX, 0, axisZ);
+        moveDirection = new Vector3(axisX, 0, axisZ).normalized;
+
         Vector3 viewDir = player.position - new Vector3(camera.transform.position.x, player.position.y, camera.transform.position.z);
         orientation.forward = viewDir.normalized;
 
@@ -51,6 +56,17 @@ public class PlayerMovement : MonoBehaviour
         {
             playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
         }
+
+        Vector3 direction = Camera.main.transform.position - player.position;
+        direction.y = 0; // Ignoriere die vertikale Komponente
+
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        //Interpoliere zwischen der aktuellen Rotation des Spielers und der Zielrotation
+        playerObj.rotation = Quaternion.Slerp(player.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+
+        moveDirection = Quaternion.AngleAxis(camera.transform.eulerAngles.y, Vector3.up) * moveDirection;
+        Debug.Log(moveDirection);
         rb.AddForce(moveDirection * moveSpeed * Time.deltaTime, ForceMode.Force);
 
         SpeedControl();

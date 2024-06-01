7acb3a720f710d13ad5a77f8d69663f977fc570b
Tim L <timlange2004@gmail.com>
2024-06-01 20:25:56 +0200

    Problem für Daniel

--- Assets/Scripts/CheckpointState.cs
+++ Assets/Scripts/CheckpointState.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public class CheckpointState : MonoBehaviour
 {
     public static CheckPoint currentCheckPoint;
-    public static int playerHp;
+    public static int playerHp = 3;
 }
